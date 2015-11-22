79727349666b6f79cca8958eac184b91a3c2a422
ael-code <tommy.ael@gmail.com>
2015-11-22 09:42:07 -0600

    users and group page

--- webant/dashboard/static/js/app/views/users-and-groups.js
+++ webant/dashboard/static/js/app/views/users-and-groups.js
@@ -20,11 +20,12 @@ define([
         el: $('#page-wrapper'),
 
         initialize: function () {
-            _.bindAll(this, "render", "populateUsersTable", "populateGroupsTable", "addUserCallBack");
+            _.bindAll(this, "render", "populateUsersTable",
+                            "populateGroupsTable", "addUserCallBack");
             this.users = new UsersCll();
-            this.users.fetch({success: this.populateUsersTable});
+            this.users.on("sync", this.populateUsersTable);
             this.groups = new GroupsCll();
-            this.groups.fetch({success: this.populateGroupsTable});
+            this.users.on("sync", this.populateGroupsTable);
         },
 
         render: function () {
@@ -32,25 +33,19 @@ define([
             this.activateForms();
             this.usersTable = this.$('#users-table').bootstrapTable();
             this.usersTable.on('click-row.bs.table', function (e, row, $element) {
-                Window.router.navigate("users/"+row.id, {trigger: true});
+                window.router.navigate("users/"+row.id, {trigger: true});
             });
             this.groupsTable = this.$('#groups-table').bootstrapTable();
             this.groupsTable.on('click-row.bs.table', function (e, row, $element) {
-                Window.router.navigate("groups/"+row.id, {trigger: true});
+                window.router.navigate("groups/"+row.id, {trigger: true});
             });
+            this.groups.fetch();
+            this.users.fetch();
         },
 
         activateForms: function() {
             this.$("#add-user-form").submit(this.addUserCallBack);
-        },
-
-        addUserCallBack: function(ev) {
-            ev.preventDefault();
-            var userElements = this.$("#add-user-form")[0].elements;
-            console.log(userElements);
-            var user = new UserM({name: userElements.username.value,
-                                        password: userElements.password.value});
-            user.save();
+            this.$("#add-user-modal").on('hidden.bs.modal', function(e){ $(e.target).find(".alert").addClass('hide');});
         },
 
         populateUsersTable: function(){
@@ -59,7 +54,27 @@ define([
 
         populateGroupsTable: function(){
             this.groupsTable.bootstrapTable('load',this.groups.toJSON());
-        }
+        },
+
+        addUserCallBack: function(ev) {
+            ev.preventDefault();
+            var userElements = this.$("#add-user-form")[0].elements;
+            this.users.create({name: userElements.username.value,
+                               password: userElements.password.value},
+                              {wait: true,
+                               success: this.hideUserModal,
+                               error: this.addUserErrorCallback});
+        },
+
+        hideUserModal: function(){
+            $("#add-user-modal").modal('hide');
+        },
+
+        addUserErrorCallback: function(model, response){
+            var alert = $("#add-user-modal .alert");
+            alert.html("<b>Error: </b>" + response.responseJSON.error.details);
+            alert.removeClass('hide');
+        },
     });
 
     return usersView;
