bfafadb1067288bacb1584ad3cb86e7288f306ab
ael-code <tommy.ael@gmail.com>
2016-03-12 19:57:29 +0100

    bugfix: webant tests were using defualt es_index name

--- webant/test/__init__.py
+++ webant/test/__init__.py
@@ -1,13 +1,20 @@
 import unittest
 from webant import create_app
 from conf.defaults import get_def_conf
+from elasticsearch import Elasticsearch
 
 
 class WebantTestCase(unittest.TestCase):
 
     def setUp(self):
-        conf = get_def_conf()
-        conf['USERS_DATABASE'] = "sqlite:///:memory:"
-        conf['PWD_ROUNDS'] = 1
-        conf['TESTING'] = True
-        self.wtc = create_app(conf).test_client()
+        self.conf = get_def_conf()
+        self.conf['USERS_DATABASE'] = "sqlite:///:memory:"
+        self.conf['PWD_ROUNDS'] = 1
+        self.conf['TESTING'] = True
+        self.conf['ES_INDEX_NAME'] = 'test_webant'
+        self.wtc = create_app(self.conf).test_client()
+
+    def tearDown(self):
+        es = Elasticsearch()
+        if es.indices.exists(self.conf['ES_INDEXNAME']):
+            es.indices.delete(self.conf['ES_INDEXNAME'])
